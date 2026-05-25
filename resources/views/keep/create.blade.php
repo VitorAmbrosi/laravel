b242a9ad3db554c23f105c71d4dee169afd080a6
Vitor Farezin Ambrosi <vitorambrosif19@gmail.com>
2026-05-25 16:58:51 -0300

    Criação de controller e views

---
+++ resources/views/keep/create.blade.php
@@ -0,0 +1,11 @@
+@extends('keep/_base')
+
+@section('conteudo')
+
+    <form method="post">
+        <textarea name="nota"></textarea>
+        <br>
+        <input type="submit" value="Gravar">
+    </form>
+
+@endsection
